c2429c638ebbe65128166b0081ad0b94724d53e6
Rohan <rohnpatil95@gmail.com>
2021-06-05 18:33:41 +0530

    added businessLayer
    part 11

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using demo.Models;
+//using demo.Models;
+using BusinessLayer;
 
 namespace demo.Controllers
 {
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Index(string departmentId)
+        //public ActionResult Index(string departmentId)
+        //{
+        //    EmployeeContext employeeContext = new EmployeeContext();
+        //    IList<Employee> employees = employeeContext.Employees.Where(emp => emp.Department.Equals(departmentId)).ToList();
+
+        //    return View(employees);
+        //}
+
+        //public ActionResult Details(string id)
+        //{
+        //    EmployeeContext employeeContext = new EmployeeContext();
+        //    Employee employee = employeeContext.Employees.Single(emp => emp.Id.Equals(id));
+
+        //    return View(employee);
+        //}
+        public ActionResult Index()
         {
-            EmployeeContext employeeContext = new EmployeeContext();
-            IList<Employee> employees = employeeContext.Employees.Where(emp => emp.Department.Equals(departmentId)).ToList();
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
 
             return View(employees);
         }
-
-        public ActionResult Details(string id)
-        {
-            EmployeeContext employeeContext = new EmployeeContext();
-            Employee employee = employeeContext.Employees.Single(emp => emp.Id.Equals(id));
-
-            return View(employee);
-        }
+        
         
     }
 }
